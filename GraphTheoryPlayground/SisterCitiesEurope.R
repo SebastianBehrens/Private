f5e750f73f67c056e54db2d1a74d6469f1629b38
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-06-14 17:59:31 +0200

    working on get_twintowns
    working on function to go and get the pairings of a domestic city (on the wiki "list of twin towns in...." page and the following html-li entries...

--- GraphTheoryPlayground/SisterCitiesEurope.R
+++ GraphTheoryPlayground/SisterCitiesEurope.R
@@ -14,15 +14,26 @@ sistertowns <- main_article %>% html_nodes("a") %>% map_df(~{
 sistertowns <- sistertowns %>% 
   filter(title %>% str_detect("List of")) %>% 
   mutate(href = paste("https://en.wikipedia.org/", href, sep = ""),
-         country = str_replace_all(title, "List of twin towns and sister cities in ", "")) %>% select(country, href)
+         country = str_replace_all(title, "List of twin towns and sister cities in ", "")) %>% select(country, href) %>% head(5)
 
 get_twintowns <- function(x){
-  x %>% read_html() %>% html_nodes("p") %>% html_nodes("b") %>% html_node("a") %>% html_attr("title")
+  x <- sistertowns[[1,2]]
+  sister_incountry <- x %>% read_html() %>% html_nodes("p") %>% html_text() %>% str_remove_all("\\W*") %>% str_remove_all("\\d")
+  aux <- x %>% read_html() %>% html_nodes("p, li") %>% html_text()
+  aux %>% head(20)
+  for(i in 1:length(aux)){
+    aux[[i]] %in% sister_incountry
+  }
+  return(list(aux))
 }
 
-sistertowns %>% group_by(country) %>% mutate(twin)
-sistertowns[[1,2]] %>% read_html() %>% html_nodes("p") %>% html_nodes("b") %>% html_node("a") %>% html_attr("title")
-aux <- sistertowns[[1,2]] %>% read_html() %>% html_nodes("li")
+sistertowns %>% rowwise() %>% mutate(sisters = get_twintowns(href)) %>% unnest(sisters)
+sistertowns[[1,2]] %>% read_html() %>% html_nodes("p") %>% html_nodes("b") %>% html_nodes("a") %>% html_attr("title")
+sistertowns[[1,2]] %>% read_html() %>% html_nodes("p, li")
+get_twintowns(sistertowns[[2,2]]) 
+
+sistertowns[[2,2]] %>% read_html() %>% html_nodes("div[id = 'mw-content-text']")  %>% 
+aux1 <-aux[[6]]
 aux[[17]] %>% html_text()
 aux %>% html_text() %>%
 main_article %>% html_nodes("a") %>% html_attr("href")
