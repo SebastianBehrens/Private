39a6b5464f2c8858ff7324457835271bc791894d
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-06-14 19:33:00 +0200

    first 'MVP'. issue with splitting on more than just one comma.
    when there are more than one comma, the latter parts are dropped... with the UK, "Scotland, UK" should be kept, i.e. the second comma should not be split...

--- GraphTheoryPlayground/SisterCitiesEurope.R
+++ GraphTheoryPlayground/SisterCitiesEurope.R
@@ -17,26 +17,36 @@ sistertowns <- sistertowns %>%
          country = str_replace_all(title, "List of twin towns and sister cities in ", "")) %>% select(country, href) %>% head(5)
 
 get_twintowns <- function(x){
-  x <- sistertowns[[1,2]]
-  sister_incountry <- x %>% read_html() %>% html_nodes("p") %>% html_text() %>% str_remove_all("\\W*") %>% str_remove_all("\\d")
-  aux <- x %>% read_html() %>% html_nodes("p, li") %>% html_text()
-  aux %>% head(20)
-  for(i in 1:length(aux)){
-    aux[[i]] %in% sister_incountry
+  # x <- sistertowns[[2,2]]
+  # getting all in-country towns that are connected to other towns
+  sister_incountry <- x %>% read_html() %>% html_nodes("p") %>% html_text() %>% str_remove_all("[\\[\\]\\d]") %>% str_trim()
+  sister_incountry <- sister_incountry[(str_length(sister_incountry) %>% between(1, 30))]
+  
+  aux <- x %>% read_html() %>% html_nodes("p, li") %>% html_text() %>% str_trim() %>% str_remove_all("[\\[\\]\\d]")
+  positions <- which(aux %in% sister_incountry)
+  begin_footnotes <- which(aux %>% str_detect("\\^")) %>% min() # final sister town right before the first footnote
+  
+  out <- tibble(sisterx = character(), sistery = character())
+  for(i in 1:length(positions)){
+    # i <- 1
+    start_position <- positions[i] + 1
+    end_position <- positions[i+1]-1
+    end_position <- ifelse(is.na(end_position), begin_footnotes - 1, end_position)
+    out <- out %>% complete(sisterx = aux[[positions[i]]], sistery = aux[start_position:end_position])
   }
-  return(list(aux))
+  return(list(out))
 }
 
-sistertowns %>% rowwise() %>% mutate(sisters = get_twintowns(href)) %>% unnest(sisters)
-sistertowns[[1,2]] %>% read_html() %>% html_nodes("p") %>% html_nodes("b") %>% html_nodes("a") %>% html_attr("title")
-sistertowns[[1,2]] %>% read_html() %>% html_nodes("p, li")
-get_twintowns(sistertowns[[2,2]]) 
 
-sistertowns[[2,2]] %>% read_html() %>% html_nodes("div[id = 'mw-content-text']")  %>% 
-aux1 <-aux[[6]]
-aux[[17]] %>% html_text()
-aux %>% html_text() %>%
-main_article %>% html_nodes("a") %>% html_attr("href")
-main_article %>% html_nodes("a") %>% html_attr("title")
-  
+sistertowns <- sistertowns %>% rowwise() %>% mutate(sisters = get_twintowns(href)) 
+sistertowns <-
+  sistertowns %>% 
+  unnest(sisters) %>% 
+  rename(country.x = country, sister.x = sisterx) %>% 
+  separate(sistery, c("sistertown.y", "countr.y"), ",\\s") %>% 
+  select(sister.x, country.x, sister.y, country.y)
 
+testing <- sistertowns %>% 
+  unnest(sisters) %>% 
+  rename(country.x = country, sister.x = sisterx)
+testing[c(152, 207, 338, 444, 451, 547, 604, 718, 818, 1031, 1114, 1272, 1296, 1336, 1373, 1377, 1389, 1411, 1448, 1460),]
