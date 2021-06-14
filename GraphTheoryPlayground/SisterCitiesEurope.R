6f8de0d85b26157591b3f0eb8ff88fab5aa3ef71
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-06-14 23:40:34 +0200

    finished

--- GraphTheoryPlayground/SisterCitiesEurope.R
+++ GraphTheoryPlayground/SisterCitiesEurope.R
@@ -14,14 +14,22 @@ sistertowns <- main_article %>% html_nodes("a") %>% map_df(~{
 sistertowns <- sistertowns %>% 
   filter(title %>% str_detect("List of")) %>% 
   mutate(href = paste("https://en.wikipedia.org/", href, sep = ""),
-         country = str_replace_all(title, "List of twin towns and sister cities in ", "")) %>% select(country, href) %>% head(5)
+         country = str_replace_all(title, "List of twin towns and sister cities in ", "")) %>% select(country, href)
+sistertowns <- sistertowns %>% filter(country != "North Cyprus")
+sistertowns <- sistertowns[c(1:45),]
 
 get_twintowns <- function(x){
-  # x <- sistertowns[[2,2]]
+  # x <- sistertowns[[1,2]]
   # getting all in-country towns that are connected to other towns
+  # output to see progress ---------------------------------
+  country <- str_remove(x, "https://en.wikipedia.org//wiki/List_of_twin_towns_and_sister_cities_in_")
+  print("Country: ---------------------------------------")
+  print(country)
+  print("         ---------------------------------------")
+  # Main ---------------------------------
   sister_incountry <- x %>% read_html() %>% html_nodes("p") %>% html_text() %>% str_remove_all("[\\[\\]\\d]") %>% str_trim()
-  sister_incountry <- sister_incountry[(str_length(sister_incountry) %>% between(1, 30))]
-  
+  # sister_incountry <- sister_incountry[(str_length(sister_incountry) %>% between(1, 30))]
+  sister_incountry <- sister_incountry[c(3:length(sister_incountry))]
   aux <- x %>% read_html() %>% html_nodes("p, li") %>% html_text() %>% str_trim() %>% str_remove_all("[\\[\\]\\d]")
   positions <- which(aux %in% sister_incountry)
   begin_footnotes <- which(aux %>% str_detect("\\^")) %>% min() # final sister town right before the first footnote
@@ -29,6 +37,9 @@ get_twintowns <- function(x){
   out <- tibble(sisterx = character(), sistery = character())
   for(i in 1:length(positions)){
     # i <- 1
+    # output to see progress ---------------------------------
+    print(aux[[positions[i]]])
+    #  ---------------------------------
     start_position <- positions[i] + 1
     end_position <- positions[i+1]-1
     end_position <- ifelse(is.na(end_position), begin_footnotes - 1, end_position)
@@ -36,17 +47,58 @@ get_twintowns <- function(x){
   }
   return(list(out))
 }
-
+# sistertowns1 <- sistertowns[c(1),] %>% rowwise() %>% mutate(sisters = get_twintowns(href)) 
+# sistertowns2 <- sistertowns[c(51:103),] %>% rowwise() %>% mutate(sisters = get_twintowns(href)) 
+# sistertowns3 <- sistertowns[c(104:154),] %>% rowwise() %>% mutate(sisters = get_twintowns(href)) 
 
 sistertowns <- sistertowns %>% rowwise() %>% mutate(sisters = get_twintowns(href)) 
 sistertowns <-
   sistertowns %>% 
   unnest(sisters) %>% 
-  rename(country.x = country, sister.x = sisterx) %>% 
-  separate(sistery, c("sistertown.y", "countr.y"), ",\\s") %>% 
-  select(sister.x, country.x, sister.y, country.y)
+  rename(country.x = country, sistertown.x = sisterx) %>% 
+  separate(sistery, c("sistertown.y", "country.y"), ",\\s", extra = "merge") %>% 
+  select(sistertown.x, country.x, sistertown.y, country.y)
 
-testing <- sistertowns %>% 
-  unnest(sisters) %>% 
-  rename(country.x = country, sister.x = sisterx)
-testing[c(152, 207, 338, 444, 451, 547, 604, 718, 818, 1031, 1114, 1272, 1296, 1336, 1373, 1377, 1389, 1411, 1448, 1460),]
+result <- sistertowns %>% 
+  filter(country.x != country.y) %>% 
+  drop_na() %>% 
+  filter(!str_detect(sistertown.x, "terminated") & !str_detect(sistertown.x, "terminated")) %>%  
+  count(country.x, country.y, sort = T)
+
+# graph <- igraph::graph_from_data_frame(result, directed = F)
+
+result_summarised <- tibble(country.x = character(), country.y = character(), count = integer())
+result <- result %>% mutate(checked = 0)
+for(i in 1:nrow(result)){
+  print("Row: --------------")
+  print(i)
+  print("     --------------")
+  # i <- 1
+  if(result[i, 4] == 0){
+  var1 <- result[[i, 1]]
+  var2 <- result[[i, 2]]
+  var3 <- result[[i, 3]]
+  result[[i, 4]] <- 1
+  var4 <- result %>% filter(checked == 0) %>% filter(country.y == var1 & country.x == var2) %>% pull(n)
+  result[result$country.y == var1 & result$country.x == var2, 4] <- 1
+  }
+  if(!is_empty(var4)){
+    var5 <- var3 + var4
+    result_summarised <- result_summarised %>% complete(country.x = var1, country.y = var2, count = var5)
+  }
+  
+}
+
+result_summarised %>% arrange(desc(count))
+result_summarised %>% filter(country.x == "Germany" & country.y == "France")
+g <- graph_from_data_frame(result_summarised, directed = F) 
+g <- delete_edges(g, E(g)[count < 250])
+g <- delete_vertices(g, degree(g) == 0)
+weights <- E(g)$count/ max(E(g)$count)
+g %>% plot(vertex.label.color = "black", 
+           edge.width = weights, edge.color = "black", layout = layout_nicely(g))
+# save(sistertowns, file = "sistertowns.Rdata")
+
+
+
+
